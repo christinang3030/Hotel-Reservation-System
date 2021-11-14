22483ffec411aef5cc061f0ffe65b93b811fc5c8
Christina Ng <68707322+christinang3030@users.noreply.github.com>
2021-11-13 21:41:18 -0800

    Update hotel_reservation.sql

--- hotel_reservation.sql
+++ hotel_reservation.sql
@@ -22,16 +22,13 @@ create table Room(rID int primary key, type text, price int);
 create table Service(rID int, serviceCode int, service text, primary key(rID), 
 foreign key(rID) references Room(rID) on update cascade on delete cascade);
 
-create table Booked(rID int, cID int, type text, checkIn date, checkOut date, PRIMARY KEY (rID), 
-foreign key (rID, type) references Room (rID, type) on update cascade on delete cascade,
+create table Booked(rID int, cID int, PRIMARY KEY (rID), date_booked date,
+foreign key (rID) references Room (rID) on update cascade on delete cascade,
 foreign key (cID) references Guest (cID) on update cascade on delete set null);
-	/* to be updated */
-    
-create table Available(rID int, type text, serviceCode int, PRIMARY KEY (rID),
-foreign key (rID, type) references Room (rID, type) on update cascade on delete cascade,
-foreign key (serviceCode) references Service (serviceCode) on update cascade on delete set null);
-	/* to be updated */
-    
+
+create table Available(rID int, PRIMARY KEY (rID), date_available date,
+foreign key (rID) references Room (rID) on update cascade on delete cascade);
+
 create table Employee(employee_ID int primary key, employee_name text, position text, phone_number text);
 
 create table Transaction(transaction_id int primary key, cID int, rID int, days int, amount_due int,
@@ -66,47 +63,70 @@ insert into Room values
 ( 202, 'single', 85 ),
 ( 203, 'double', 95 ),
 ( 204, 'double', 115 ),
-( 205, 'triple', 130 );
+( 205, 'triple', 130 ),
+( 301, 'single', 80 ),
+( 302, 'single', 80 ),
+( 303, 'double', 105 ),
+( 304, 'double', 120 ),
+( 305, 'triple', 125 );
 
 	/* 001: special meal, 002: minibar, 004: spa */
-insert into Service values (101, 002, 'minibar'),
-                    (102, 004, 'spa'),
-                    (103, 001, 'special meal'),
-                    (104, 004, 'spa'),
-                    (105, 002, 'minibar'),
-                    (201, 004, 'spa'),
-                    (202, 004, 'spa'),
-                    (203, 001, 'special meal'),
-                    (204, 001, 'special meal'),
-                    (205, 002, 'minibar');
+insert into Service values 
+(101, 002, 'minibar'),
+(102, 004, 'spa'),
+(103, 001, 'special meal'),
+(104, 004, 'spa'),
+(105, 002, 'minibar'),
+(201, 004, 'spa'),
+(202, 004, 'spa'),
+(203, 001, 'special meal'),
+(204, 001, 'special meal'),
+(205, 002, 'minibar'),
+(301, 001, 'special meal'),
+(302, 001, 'special meal'),
+(303, 002, 'minibar'),
+(304, 002, 'minibar'),
+(305, 001, 'special meal');
 
-/*
 insert into Booked values
-	/* to be updated */
-/*
+(101, 1001, '2021-10-01'),
+(102, 1002, '2021-10-01'),
+(103, 1003, '2021-10-01'),
+(201, 1004, '2021-10-01'),
+(203, 1005, '2021-10-01'),
+(301, 1006, '2021-10-01'),
+(305, 1010, '2021-10-01');
+
 insert into Available values
-	/* to be updated */
+(104, '2021-10-01'),
+(105, '2021-10-01'),
+(202, '2021-10-01'),
+(204, '2021-10-01'),
+(205, '2021-10-01'),
+(302, '2021-10-01'),
+(303, '2021-10-01'),
+(304, '2021-10-01');
 
 insert into Employee values
-					( 0, 'Arnoldo Demetrio', 'Concierge', '555-1234' ),
-					( 1, 'Lila Prudenzio', 'Manager', '555-1111' ),
-                    ( 2, 'Stig Tahmina', 'Front Desk', '555-1112' ),
-                    ( 3, 'Erhan Jozafat', 'Valet', '555-1121' ),
-                    ( 4, 'Eini Finn', 'Janitor', '555-1211' ),
-                    ( 5, 'Melanija Javiera', 'Sales', '555-1311' ),
-                    ( 6, 'Suman Gundisalvus', 'Housekeeper', '555-1131' ),
-                    ( 7, 'Solly Ivanka', 'Security', '555-1114' );
+( 0, 'Arnoldo Demetrio', 'Concierge', '555-1234' ),
+( 1, 'Lila Prudenzio', 'Manager', '555-1111' ),
+( 2, 'Stig Tahmina', 'Front Desk', '555-1112' ),
+( 3, 'Erhan Jozafat', 'Valet', '555-1121' ),
+( 4, 'Eini Finn', 'Janitor', '555-1211' ),
+( 5, 'Melanija Javiera', 'Sales', '555-1311' ),
+( 6, 'Suman Gundisalvus', 'Housekeeper', '555-1131' ),
+( 7, 'Solly Ivanka', 'Security', '555-1114' );
                     
 /* Making irrelevant information since this will be active data, has no bearance on the current available or booked entries */
 insert into Transaction values
-					( 0, 1001, 101, 3, 235, '2021-10-11 10:34:09', '2021-10-11', '2021-10-14' ),
-                    ( 1, 1002, 102, 1, 75, '2021-11-11 22:34:09', '2021-11-11', '2021-11-12' ),
-                    ( 2, 1003, 103, 2, 155, '2021-10-11 5:22:09', '2021-10-12', '2021-10-14' ),
-                    ( 3, 1004, 104, 5, 455, '2021-05-11 10:34:09', '2021-05-11', '2021-05-16' ),
-                    ( 4, 1005, 105, 2, 125, '2021-10-11 10:34:09', '2021-10-11', '2021-10-13' ),
-                    ( 5, 1006, 201, 3, 345, '2021-10-11 10:34:09', '2021-10-11', '2021-10-14' ),
-                    ( 6, 1007, 202, 4, 535, '2021-10-11 10:34:09', '2021-10-11', '2021-10-15' ), 
-                    ( 7, 1008, 203, 5, 735, '2021-10-11 10:34:09', '2021-10-11', '2021-10-16' );
+( 0, 1001, 101, 3, 235, '2021-10-11 10:34:09', '2021-10-11', '2021-10-14' ),
+( 1, 1002, 102, 1, 75, '2021-11-11 22:34:09', '2021-11-11', '2021-11-12' ),
+( 2, 1003, 103, 2, 155, '2021-10-11 5:22:09', '2021-10-12', '2021-10-14' ),
+( 3, 1004, 104, 5, 455, '2021-05-11 10:34:09', '2021-05-11', '2021-05-16' ),
+( 4, 1005, 105, 2, 125, '2021-10-11 10:34:09', '2021-10-11', '2021-10-13' ),
+( 5, 1006, 201, 3, 345, '2021-10-11 10:34:09', '2021-10-11', '2021-10-14' ),
+( 6, 1007, 202, 4, 535, '2021-10-11 10:34:09', '2021-10-11', '2021-10-15' ), 
+( 7, 1008, 203, 5, 735, '2021-10-11 10:34:09', '2021-10-11', '2021-10-16' );
 
 /* TRIGGERS */
 drop trigger if exists ServiceD;
@@ -116,9 +136,10 @@ after delete on Room
 for each row 
 begin 
 	delete from Service 
-	where number = old.rID; 
+	where rID = old.rID; 
 end;
 //
+
 
 DROP TRIGGER IF EXISTS BookedTrigger;
 delimiter //
@@ -127,7 +148,7 @@ AFTER INSERT ON Booked
 FOR EACH ROW 
 BEGIN
     DELETE FROM Available
-    WHERE number = OLD.rID;
+    WHERE rID = new.rID;
 END;
 //
 
@@ -138,38 +159,87 @@ AFTER INSERT ON Available
 FOR EACH ROW
 BEGIN
     DELETE FROM Booked
-    WHERE number = OLD.rID;
+    WHERE rID = new.rID;
 END;
 //
 
-drop trigger if exists InsertGuest;
+DROP TRIGGER IF EXISTS DelFromBooked//
 delimiter //
-create trigger InsertGuest
-after insert on Guest
-for each row
-begin
-	update Guest
-    set updatedAt=current_date();
-end;
+CREATE TRIGGER DelFromBooked
+AFTER DELETE ON Booked
+FOR EACH ROW
+BEGIN
+    INSERT INTO Available values (OLD.rID, CURRENT_DATE());
+END;
 //
 
-drop trigger if exists UpdateGuest;
+DROP TRIGGER IF EXISTS DelFromAvail//
 delimiter //
-create trigger UpdateGuest
-after update on Guest
-for each row
-begin
-	update Guest
-    set updatedAt=current_date();
-end;
+CREATE TRIGGER DelFromAvail
+AFTER DELETE ON Available
+FOR EACH ROW
+BEGIN
+	INSERT INTO Booked values (OLD.rID, CURRENT_DATE());
+END;
 //
 
-/* ARCHIVE FUNCTION */
-drop procedure if exists Archive;
+/* ARCHIVE PROCEDURE */
+DROP PROCEDURE IF EXISTS Archive;
 delimiter //
-create procedure Archive(in cutoff date)
-begin
+CREATE PROCEDURE Archive(IN cutoff date)
+BEGIN
 	insert into ArchivedGuest (select * from Guest where updatedAt < cutoff);
     delete from Guest where updatedAt < cutoff;
-end;
+END;
 //
+
+
+/* FUNCTIONAL REQUIREMENTS */
+
+/* Get list of guests in alphabetical order */
+select cID, name, age from Guest order by name;
+
+/* Find the cheapest double room */
+select * from Room where type='double' and price <= all (select price from Room R2 where type='double');
+
+/* Sort the rooms under $100 from cheapest to most expensive */
+select * from Room where price < 100 order by price, rID;
+
+/* Find types of available rooms where the maximum price is 100 */
+select type, max(price) as max_price from Available join Room where Available.rID = Room.rID group by type having max(price) <= 100;
+
+/* Need to display all employees for managers or HR, etc */
+select * from Employee;
+
+/* Select transactions where the check in date is in the future */
+select * from Transaction where checkIn > curdate();
+
+/* Someone needs to request room cleaning */
+select * from Employee where position like 'housekeeper'; 
+
+/* Check customer transaction history */
+select * from Transaction where cID in ( select cID from Guest where name like 'customer name here');
+
+/* Find all available single rooms: */
+select * from Available where rID in (select rID from Room where type="single");
+
+/* Find all available rooms with more than one bed: */
+select * from Available where rID in (select rID from Room where type <> "single");
+
+/* Find all available rooms that come with a spa: */
+select * from Available where rID in (select Room.rID from Room, Service where Room.rID=Service.rID and service="spa");
+
+/* Find all booked rooms with guests over 50: */
+select * from Booked where cID in (select cID from Guest where age > 50);
+
+/* Find all available rooms that request minibar for an extra service: */
+select * from Room where rID in ( select Service.rID from Service, Available where Service.rID=Available.rID and service = 'minibar');
+
+/* Find all available rooms that request spa or special meal for an extra service: */
+select * from Room where rID in ( select Service.rID from Service, Available where Service.rID=Available.rID and (service = 'spa' or service = 'special meal'));
+
+/* Find all single rooms that request spa for an extra service: */
+select * from Room where rID in (select rID from Service where Room.rID = Service.rID and Room.type = 'single' and Service.service = 'spa');
+
+/* Find all available service with triple room : */
+select distinct service from Service where rID in (select rID from Room where Room.rID = Service.rID and Room.type = 'triple');
