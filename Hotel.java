ab358fea18fb2436797c699d7bf40fc518c08085
Christina Ng <68707322+christinang3030@users.noreply.github.com>
2021-12-04 21:56:50 -0800

    Update Hotel.java

--- Hotel.java
+++ Hotel.java
@@ -1,4 +1,5 @@
 import java.sql.*;
+import java.util.concurrent.TimeUnit;
 
 public class Hotel {
 
@@ -6,7 +7,7 @@ public class Hotel {
     static final String DB_URL = "jdbc:mysql://localhost:3306/hotel_reservation";
     //  Database credentials
     static final String USER = "root";
-    static final String PASS = "password";
+    static final String PASS = "shuju888";
     private static Connection conn = null;
     private static Statement statement = null;
 
@@ -45,15 +46,38 @@ public class Hotel {
 
             // Functional requirements
             System.out.println("\n\n====== Functional Requirements ======");
-            guestsInAlphaOrder();
-            findCheapestDouble();
-            sortUnder100();
-            typesMax100();
-            allEmployees();
-            futureTransactions();
-            roomCleaning();
+            guestsInAlphaOrder(statement);
+            findCheapestDouble(statement);
+            sortUnder100(statement);
+            typesMax100(statement);
+            allEmployees(statement);
+            futureTransactions(statement);
+            roomCleaning(statement);
+            checkTransactionHistory(statement);
+            findSingleRooms(statement);
+            findNonSingleRooms(statement);
+            findSpaRooms(statement);
+            findOlderBooked(statement);
+            findMinibarRooms(statement);
+            findSpaOrMealRooms(statement);
+            findSingleSpaRooms(statement);
+            findTripleRoomServices(statement);
 
             // Key constraint violations
+            System.out.println("\n\n====== Primary Key Constraint Violation ======");
+            /*try {
+                primaryKeyViolation(statement);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }*/
+
+            System.out.println("\n\n^^^ The error message may appear above for the foreign key constraint ^^^");
+            System.out.println("\n====== Foreign Key Constraint Violation ======");
+            try {
+                foreignKeyViolation(statement);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
 
             // Archive function
             System.out.println("\n\n====== Archive funcion ======");
@@ -163,9 +187,7 @@ public class Hotel {
         }
     }
 
-    private static void guestsInAlphaOrder() throws SQLException {
-        conn = DriverManager.getConnection(DB_URL + "?serverTimezone=UTC", USER, PASS);
-        statement = conn.createStatement();
+    private static void guestsInAlphaOrder(Statement statement) throws SQLException {
         ResultSet rs = statement.executeQuery("select * from Guest order by name");
         System.out.println("\nGetting a lit of all guests in alphabetical order:");
         //printResultSetfromGuest(rs);
@@ -177,26 +199,25 @@ public class Hotel {
         }
     }
 
-    private static void findCheapestDouble() throws SQLException {
-        conn = DriverManager.getConnection(DB_URL + "?serverTimezone=UTC", USER, PASS);
-        statement = conn.createStatement();
-        ResultSet rs = statement.executeQuery("select * from Room where type='double' and price <= all (select price from Room R2 where type='double')");
+    private static void findCheapestDouble(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery(
+                "select * from Room where type='double' " +
+                        "and price <= all (select price from Room R2 where type='double')");
         System.out.println("\nFinding the cheapest double room:");
         printResultSetfromRoom(rs);
     }
 
-    private static void sortUnder100() throws SQLException {
-        conn = DriverManager.getConnection(DB_URL + "?serverTimezone=UTC", USER, PASS);
-        statement = conn.createStatement();
-        ResultSet rs = statement.executeQuery("select * from Room where price < 100 order by price, rID");
+    private static void sortUnder100(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery
+                ("select * from Room where price < 100 order by price, rID");
         System.out.println("\nSorting the rooms under $100 from cheapest to most expensive:");
         printResultSetfromRoom(rs);
     }
 
-    private static void typesMax100() throws SQLException {
-        conn = DriverManager.getConnection(DB_URL + "?serverTimezone=UTC", USER, PASS);
-        statement = conn.createStatement();
-        ResultSet rs = statement.executeQuery("select type, max(price) as max_price from Available join Room where Available.rID = Room.rID group by type having max(price) <= 100");
+    private static void typesMax100(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery
+                ("select type, max(price) as max_price from Available join Room " +
+                        "where Available.rID = Room.rID group by type having max(price) <= 100");
         System.out.println("\nFinding the types of available rooms where the max price is $100:");
         while (rs.next()) {
             String type = rs.getString("type");
@@ -205,29 +226,115 @@ public class Hotel {
         }
     }
 
-    private static void allEmployees() throws SQLException {
-        conn = DriverManager.getConnection(DB_URL + "?serverTimezone=UTC", USER, PASS);
-        statement = conn.createStatement();
+    private static void allEmployees(Statement statement) throws SQLException {
         ResultSet rs = statement.executeQuery("select * from Employee");
         System.out.println("\nFinding all employees:");
         printResultSetfromEmployee(rs);
     }
 
-    private static void futureTransactions() throws SQLException
+    private static void futureTransactions(Statement statement) throws SQLException
     {
-        conn = DriverManager.getConnection(DB_URL + "?serverTimezone=UTC", USER, PASS);
-        statement = conn.createStatement();
         ResultSet rs = statement.executeQuery("select * from Transaction where checkIn > \"2021-10-11\"");
         System.out.println("\nFinding all transactions where the check in date is after '2021-10-11':");
         printResultSetfromTransaction(rs);
     }
 
-    private static void roomCleaning() throws SQLException
+    private static void roomCleaning(Statement statement) throws SQLException
     {
-        conn = DriverManager.getConnection(DB_URL + "?serverTimezone=UTC", USER, PASS);
-        statement = conn.createStatement();
         ResultSet rs = statement.executeQuery("select * from Employee where position like 'housekeeper'");
         System.out.println("\nFinding people for room cleaning:");
         printResultSetfromEmployee(rs);
     }
+
+    private static void checkTransactionHistory(Statement statement) throws SQLException
+    {
+        ResultSet rs = statement.executeQuery("select * from Transaction where cID in " +
+                                        "( select cID from Guest where name like 'Austin Lee');");
+        System.out.println("\nChecking customer (Austin Lee)'s transaction history:");
+        printResultSetfromTransaction(rs);
+    }
+
+    private static void findSingleRooms(Statement statement) throws SQLException {
+        System.out.println("\nFinding all available single rooms:");
+        ResultSet rs = statement.executeQuery("select * from Available where rID in " +
+                                                "(select rID from Room where type = \"single\")");
+        printResultSetfromAvailable(rs);
+    }
+
+    private static void findNonSingleRooms(Statement statement) throws SQLException {
+        System.out.println("\nFinding all available rooms with more than one bed:");
+        ResultSet rs = statement.executeQuery("select * from Available where rID in " +
+                                                "(select rID from Room where type <> \"single\")");
+        printResultSetfromAvailable(rs);
+    }
+
+    private static void findSpaRooms(Statement statement) throws SQLException {
+        System.out.println("\nFinding all available rooms that come with a spa:");
+        ResultSet rs = statement.executeQuery("select * from Available where rID in " +
+                "(select Room.rID from Room, Service where Room.rID=Service.rID and service=\"spa\")");
+        printResultSetfromAvailable(rs);
+    }
+
+    private static void findOlderBooked(Statement statement) throws SQLException {
+        System.out.println("\nFinding all booked rooms with guests over 50:");
+        ResultSet rs = statement.executeQuery("select * from Booked where cID in " +
+                                                "(select cID from Guest where age > 50)");
+        printResultSetfromBooked(rs);
+    }
+
+    private static void findMinibarRooms(Statement statement) throws SQLException {
+        System.out.println("\nFinding all available rooms with a minibar:");
+        ResultSet rs = statement.executeQuery("select * from Room where rID in " +
+                        "( select Service.rID from Service, Available " +
+                        "where Service.rID=Available.rID and service = 'minibar')");
+        printResultSetfromRoom(rs);
+    }
+
+    private static void findSpaOrMealRooms(Statement statement) throws SQLException {
+        System.out.println("\nFinding all available rooms with a spa or special meal:");
+        ResultSet rs = statement.executeQuery("select * from Room where rID in " +
+                "( select Service.rID from Service, Available " +
+                "where Service.rID=Available.rID and (service = 'spa' or service = 'special meal'))");
+        printResultSetfromRoom(rs);
+    }
+
+    private static void findSingleSpaRooms(Statement statement) throws SQLException {
+        System.out.println("\nFinding all single rooms that request spa for an extra service:");
+        ResultSet rs = statement.executeQuery("select * from Room where rID in " +
+                "(select rID from Service where Room.rID = Service.rID and Room.type = 'single' " +
+                "and Service.service = 'spa')");
+        printResultSetfromRoom(rs);
+    }
+
+    private static void findTripleRoomServices(Statement statement) throws SQLException {
+        System.out.println("\nFinding all available services with triple rooms:");
+        ResultSet rs = statement.executeQuery("select distinct service from Service where rID in " +
+                "(select rID from Room where Room.rID = Service.rID and Room.type = 'triple')");
+
+        while(rs.next())
+        {
+            String service = rs.getString("service");
+            System.out.println("Service: " + service);
+        }
+    }
+
+    private static void primaryKeyViolation(Statement statement) throws SQLException {
+        ResultSet rs = statement.executeQuery("SELECT * FROM Room WHERE rID=101");
+        System.out.println("Find if room 101 exists:");
+        printResultSetfromRoom(rs);
+
+        System.out.println("Attempt to execute 'INSERT INTO Room VALUES (101, 'single', 100)'");
+        statement.executeUpdate("INSERT INTO Room VALUES (101, 'single', 100)");
+    }
+
+    private static void foreignKeyViolation(Statement statement) throws SQLException {
+        System.out.println("Find if room 101 is booked");
+        ResultSet rs = statement.executeQuery("SELECT * FROM Booked WHERE rID=101");
+        printResultSetfromBooked(rs);
+
+        System.out.println("Attempt to execute 'DELETE FROM Room WHERE rID=101'");
+        statement.executeUpdate("DELETE FROM Room WHERE rID=101");
+    }
 }
+
+
